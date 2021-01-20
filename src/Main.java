b77229da9c49faa6acebda5a5c671934b3aa3d8c
Elena Proskurina <friklen@gmail.com>
2021-01-20 20:51:15 +0400

    milesBonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        long ticket = 1_500;
+        int Miles = 20;
+        long amount = ticket / Miles;
+        System.out.println(amount);
+
+    }
+
+}
